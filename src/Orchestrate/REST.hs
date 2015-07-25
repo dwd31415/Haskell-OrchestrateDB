b2e4c954b525bf60fbe2e833d742d37cd16650f9
Adrian Dawid <adriandwd@gmail.com>
2015-07-25 12:32:01 +0200

    Added GET and DELETE functions

--- src/Orchestrate/REST.hs
+++ src/Orchestrate/REST.hs
@@ -4,12 +4,13 @@
 module Orchestrate.REST
     (
       validateApplication,
-      orchestrateCollectionPut
+      orchestrateCollectionPutWithoutKey,
+      orchestrateCollectionDelete,
+      orchestrateCollectionGet
     ) where
 
 import Network.HTTP.Conduit
 import Network.HTTP.Types.Status
-import Network.HTTP.Types.Header
 
 import qualified Data.ByteString.Lazy as BSLazy
 import qualified Data.ByteString.Char8 as B
@@ -39,8 +40,8 @@ validateApplication application = do
                         else return False
                       Left (_::X.SomeException) -> return False
 
-orchestrateCollectionPut :: ToJSON obj => OrchestrateApplication -> OrchestrateCollection -> obj -> IO Bool
-orchestrateCollectionPut application collection object = do
+orchestrateCollectionPutWithoutKey :: ToJSON obj => OrchestrateApplication -> OrchestrateCollection -> obj -> IO Bool
+orchestrateCollectionPutWithoutKey application collection object = do
   let objAsJson = encode object
   let api_key = apiKey application
   if api_key == ""
@@ -59,6 +60,51 @@ orchestrateCollectionPut application collection object = do
                   resOrException <- X.try (http reqHead manager)
                   case resOrException of
                     Right res -> if responseStatus res == status201
+                      then return True
+                      else return False
+                    Left (_::X.SomeException) -> return False
+
+orchestrateCollectionGet :: FromJSON res => OrchestrateApplication -> OrchestrateCollection -> String -> IO (Maybe res)
+orchestrateCollectionGet application collection key = do
+  let api_key = apiKey application
+  if api_key == ""
+    then return Nothing
+    else do
+      let url = httpsEndpoint application ++ "/" ++ collectionName collection ++ "/" ++ key
+      case parseUrl url of
+        Nothing -> return Nothing
+        Just unsecuredRequest -> withManager $ \manager -> do
+                let request = unsecuredRequest {
+                                        method = "GET",
+                                        secure = True }
+                let reqHead = applyBasicAuth (B.pack $ apiKey application) "" request
+                resOrException <- X.try (httpLbs reqHead manager)
+                case resOrException of
+                  Right res -> if responseStatus res == ok200
+                    then do
+                      let resBody = responseBody res
+                      let jsonObject = decode resBody
+                      return jsonObject
+                    else return  Nothing
+                  Left (_::X.SomeException) -> return  Nothing
+
+orchestrateCollectionDelete :: OrchestrateApplication -> OrchestrateCollection -> IO Bool
+orchestrateCollectionDelete application collection = do
+  let api_key = apiKey application
+  if api_key == ""
+  then return False
+  else do
+    let url = httpsEndpoint application ++ "/" ++ collectionName collection ++ "?force=true"
+    case parseUrl url of
+      Nothing -> return False
+      Just unsecuredRequest -> withManager $ \manager -> do
+                  let request = unsecuredRequest {
+                                          method = "DELETE",
+                                          secure = True }
+                  let reqHead = applyBasicAuth (B.pack $ apiKey application) "" request
+                  resOrException <- X.try (http reqHead manager)
+                  case resOrException of
+                    Right res -> if responseStatus res == status204
                       then return True
                       else return False
                     Left (_::X.SomeException) -> return False
